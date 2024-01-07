42ed2e1577bea742aa7c9747609ee3048bcd8870
Munkhbadral Altanpurev <15154972+mbadrala@users.noreply.github.com>
2024-01-07 12:41:57 +0800

    add nvim

---
+++ nvim/after/plugin/colors.lua
@@ -0,0 +1,10 @@
+function ColorMyPencils(color)
+    color = color or "solarized-osaka-storm"
+    vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+end
+
+ColorMyPencils()
