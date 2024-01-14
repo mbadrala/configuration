de9051c5efd32b6962d5d491cbe8075b8fe9fc40
Munkhbadral Altanpurev <15154972+mbadrala@users.noreply.github.com>
2024-01-15 01:42:50 +0800

    update neovim, tmux

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -12,3 +12,8 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>pl', builtin.live_grep, {})
+
+vim.keymap.set('n', '<leader>pgs', builtin.git_status, {})
+vim.keymap.set('n', '<leader>pgc', builtin.git_commits, {})
--- .config/nvim/lua/mbadrala/set.lua
+++ .config/nvim/lua/mbadrala/set.lua
@@ -14,7 +14,8 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME" .. "/.vim/undodir")
 vim.opt.undofile = true
 
-vim.opt.wrap = false
+vim.opt.wrap = true 
+vim.opt.linebreak = true 
 
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
