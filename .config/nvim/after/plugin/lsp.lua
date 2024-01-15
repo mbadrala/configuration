cd284580a4c2ee6d6f819009bcaf0834ddc604e1
Munkhbadral Altanpurev <15154972+mbadrala@users.noreply.github.com>
2024-01-15 08:16:27 +0800

    add lsp, treesitter

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,22 +1,60 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    client.server_capabilities.semanticTokensProvider = nil
+    lsp_zero.default_keymaps({buffer = bufnr})
 end)
+
+local on_attach
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {
-      "rust_analyzer",
-      "pyright",
-      "gopls",
-      "clangd",
-      "tsserver",
-      "wgsl_analyzer"
-  },
-  handlers = {
-    lsp_zero.default_setup,
-  },
+    ensure_installed = { "lua_ls", "gopls", "clangd", "tsserver", "pyright", "omnisharp" },
+    handlers = {
+        lsp_zero.default_setup,
+        omnisharp = function ()
+            require('lspconfig').omnisharp.setup({
+                handlers = {
+                    ["textDocument/definition"] = require('omnisharp_extended').handler,
+                },
+                cmd = { "omnisharp", '--languageserver', '--hostPID', tostring(vim.fn.getpid()) },
+                enable_editorconfig_support = true, -- setting from .editorconfig
+                enable_ms_build_load_projects_on_demand = false,
+                enable_roslyn_analyzers = true,
+                analyze_open_documents_only = true,
+                organize_imports_on_format = true,
+                enable_import_completion = false,
+                sdk_include_prereleases = true,
+            })
+        end
+    },
 })
+
+local cmp = require('cmp')
+local cmp_action = lsp_zero.cmp_action()
+
+cmp.setup({
+    mapping = cmp.mapping.preset.insert({
+        -- `Enter` key to confirm completion
+        ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+        -- Ctrl+Space to trigger completion menu
+        ['<C-Space>'] = cmp.mapping.complete(),
+
+        -- Navigate between snippet placeholder
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+        -- Scroll up and down in the completion documentation
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    })
+})
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = false
+    }
+)
--- .config/nvim/lua/mbadrala/packer.lua
+++ .config/nvim/lua/mbadrala/packer.lua
@@ -9,8 +9,33 @@ return require('packer').startup(function(use)
         -- or                            , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
     use ('mbbill/undotree')
     use ('tpope/vim-fugitive')
     use ('norcalli/nvim-colorizer.lua')
+    use "Hoffs/omnisharp-extended-lsp.nvim"
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- Uncomment the two plugins below if you want to manage the language servers from neovim
+            --- and read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+        }
+    }
 end)
 
